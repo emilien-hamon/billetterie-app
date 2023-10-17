1b99a1e5bc2380d653f629d4c0acacaba1d08dd9
Emilien <emilienhamon@outlook.com>
2023-10-17 14:36:22 +0200

    billetterie-app
    modif mineur

--- resources/views/salle/edit.blade.php
+++ resources/views/salle/edit.blade.php
@@ -8,17 +8,17 @@
 
         <div class="form-group">
             <label for="nom" class="text-white">{{ __('Nom') }}</label>
-            <input type="text" name="nom" id="nom" class="form-control" required maxlength="75" value="{{ old('nom', $salle->nom) }}">
+            <input placeholder="{{__('Nom')}}" type="text" name="nom" id="nom" class="form-control" required maxlength="75" value="{{ old('nom', $salle->nom) }}">
         </div>
 
         <div class="form-group">
             <label for="adresse" class="text-white">{{ __('Adresse') }}</label>
-            <input type="text" name="adresse" id="adresse" class="form-control" placeholder="{{ __('Adresse') }}" required maxlength="75" value="{{ old('adresse', $salle->adresse) }}">
+            <input placeholder="{{__('Adresse')}}" type="text" name="adresse" id="adresse" class="form-control" placeholder="{{ __('Adresse') }}" required maxlength="75" value="{{ old('adresse', $salle->adresse) }}">
         </div>
 
         <div class="form-group">
             <label for="place" class="text-white">{{ __('Nombre de places') }}</label>
-            <input type="number" name="place" id="place" class="form-control" placeholder="{{ __('Nombre de place') }}" required value="{{ old('place', $salle->place) }}">
+            <input placeholder="{{__('Nombre de place')}}" type="number" name="place" id="place" class="form-control" placeholder="{{ __('Nombre de place') }}" required value="{{ old('place', $salle->place) }}">
         </div>
 
         <div class="mt-4">
