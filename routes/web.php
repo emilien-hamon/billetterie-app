7e70970e1e33b1afec821102d67aeeee3a7cd4e4
Emilien <emilienhamon@outlook.com>
2023-11-28 10:33:50 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AccueilController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\SalleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +19,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::resource('salle', SalleController::class);
+
+    Route::resource('client', ClientController::class);
+
+    Route::resource('reservation', ReservationController::class);
+    Route::get('/dashboard', 'DashboardController@index');
+    // ...
+});
+
+Route::get('/login', 'Auth\LoginController@index');
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('accueil');
+})->middleware(['auth', 'verified'])->name('accueil');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/change-language', [LanguageController::class, 'changeLanguage'])->name('change.language');
 
 require __DIR__.'/auth.php';
